ec1b7aa249d9bcc1bf751dc58fe0a3b9343c2780
captaincrazybro <myeyesareonmizar@gmail.com>
2021-11-11 17:49:34 -0500

    Added markers

--- main.py
+++ main.py
@@ -44,16 +44,20 @@ def main():
     board = BoardShim(board_id, params)
     board.prepare_session()
 
-    # board.start_stream () # use this for default options
     board.start_stream(45000, streamerparams)
-    time.sleep(10)
-    #data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
-    data = board.get_board_data()  # get all data and remove it from internal buffer
+    for i in range(10):
+        time.sleep(1)
+        board.insert_marker(i + 1)
+    data = board.get_board_data()
     board.stop_stream()
     board.release_session()
 
+    print_to_file(data)
+
+
+def print_to_file(arrs):
     file = open("log.txt", "w")
-    for arr in data:
+    for arr in arrs:
         str = "["
         for element in arr:
             str += f'{element}, '
