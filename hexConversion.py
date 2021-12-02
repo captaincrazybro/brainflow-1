98463dfe0fb3b9ff89980f3c5832d89a83c02f26
cbortle <cbortle@highpoint.edu>
2021-12-02 17:48:11 -0500

    Program to convert decimal to hex, eventually the  goal is hex to colors

---
+++ hexConversion.py
@@ -0,0 +1,26 @@
+#Program to convert decimal values from data, into Hexadecimal Values
+
+import binhex
+def main():
+    with open('log.txt') as f:
+        lines = f.readlines()
+
+        data = []
+
+        for i in range (data):
+            result = hex(data[i])
+            print (result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()
